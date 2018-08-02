c9969b934fa013b81cdf591101e63d63c8e6e9ef
kauhiant <kauhia3440@gmail.com>
2018-08-02 21:00:14 +0800

    [comment for team meamber] SkillManager

--- Assets/Maze/SkillManager.cs
+++ Assets/Maze/SkillManager.cs
@@ -8,8 +8,10 @@ namespace Maze
 {
     public class SkillManager
     {
+        // 目前在場上出現的技能效果.
         private static List<GameObject> skillObjs = new List<GameObject>();
 
+        // 將場上所有技能效果清除.
         public static void clear()
         {
             while(skillObjs.Count != 0)
@@ -19,39 +21,69 @@ namespace Maze
             }
         }
 
+        // [以施術者為主，需要轉向]
+        // 在場上顯示技能效果.(會轉向，會縮放)
+        // userPosition 施術者位置.
+        // userVector   施術者面向的方位.
+        // depthScale   技能效果的深度.
+        // widthScale   技能效果的寬度.
+        // skill        技能效果的圖片.
+        // 回傳         技能效果的GameObject.
         private static GameObject showSkill(Vector2 userPosition, Vector2 userVector, int depthScale, int widthScale, Sprite skill)
         {
+            // 如果這個技能玩家不該看到，就不顯示.
             if (userVector.Equals(Vector2.zero)) return null;
 
+            // 創造一個物件.
             float dist = depthScale / 2f + 0.5f;
             GameObject skillObj = new GameObject();
+
+            // 讓他移動到指定位置.
             skillObj.transform.position = userPosition;
             skillObj.transform.Translate(userVector * dist);
+
+            // 放大到適當尺寸.
             skillObj.transform.localScale = new Vector2(depthScale, widthScale);
 
+            // 將這個物件的圖片設定為指定的圖片，並放在技能專屬的圖層.
             skillObj.AddComponent<SpriteRenderer>().sprite = skill;
             skillObj.GetComponent<SpriteRenderer>().sortingLayerName = "action";
+
+            // 轉向.
             skillObj.transform.Rotate(angle(userVector));
 
+            // 將此物件加入[目前場上出現的技能效果]這個List.
             skillObjs.Add(skillObj);
             return skillObj;
         }
 
+        // [以一個點為主，不需要轉向]
+        // 在場上顯示技能效果.(在一個點顯示一張圖片)
+        // position 技能效果的位置.
+        // skill    技能效果的圖片.
+        // 回傳     技能效果的GameObject.
         private static GameObject showSkill(Vector2 position, Sprite skill)
         {
+            // 在一個位置創造一個物件.
             GameObject skillObj = new GameObject();
             skillObj.transform.position = position;
             
+            // 將這個物件的圖片設定為指定的圖片，並放在技能專屬的圖層.
             skillObj.AddComponent<SpriteRenderer>().sprite = skill;
             skillObj.GetComponent<SpriteRenderer>().sortingLayerName = "action";
           
+            // 將此物件加入[目前場上出現的技能效果]這個List.
             skillObjs.Add(skillObj);
             return skillObj;
         }
 
 
 
-
+        // [給 Animal 或 Creater (施術者) 使用]
+        // 在場上顯示技能效果.
+        // skill        技能名稱.
+        // userPosition 施術者位置.
+        // userVector   施術者面向方位.
         public static void showSkill(Skill skill, Point2D userPosition, Vector2D userVector) 
         {
             if (!userPosition.IsOnPlain(GlobalAsset.player.plain))
@@ -79,7 +111,7 @@ namespace Maze
 
 
 
-
+        // GameObject 轉向用.
         private static Vector3 angle(Vector2 vector)
         {
             if (vector.Equals(Vector2.right))
@@ -94,11 +126,13 @@ namespace Maze
                 return Vector3.zero;
         }
 
+        // 這她媽最好看不懂.
         private static Vector2 convert(Point2D point)
         {
             return new Vector2(point.X.value, point.Y.value);
         }
 
+        // 這也是.
         private static Vector2 convert(Vector2D vector)
         {
             switch (vector)
